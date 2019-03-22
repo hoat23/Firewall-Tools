d7ced73552f4079be614b5bc2f19b2470ee0d662
Deiner Zapata <deinerzapata@gmail.com>
2019-03-22 15:57:47 -0500

    Add files via upload

--- multiprocess_monitor_firewall_ssh.py
+++ multiprocess_monitor_firewall_ssh.py
@@ -13,11 +13,11 @@ import os, signal, sys
 from monitor_firewall_ssh import *
 from dictionary import *
 #########################################################################################
-def execute_process(ip, port, logstash, return_dict, username, password, num_int=4):
+def execute_process(ip, port, logstash, list_vdom, return_dict, username, password, num_int=4):
     command = "check_process,sysinfo_shm,sysinfo_memory,sysinfo_conserve"
     intent = 1
     while (intent <= num_int):
-        res = get_data_firewall_ssh(command, ip, port, username, password, logstash=logstash)
+        res = get_data_firewall_ssh(command, ip, port, username, password, logstash=logstash, vdom=list_vdom)
         if res['status']!="error" :
             break
         #sleep(1)
@@ -60,15 +60,20 @@ def launch_watchdog(list_process, dict_pid_ip, logstash, return_dict, username, 
         sleep(2)
         print("Nª           IP                 PID       RUN\tINT\tSTATUS\t ENL_TIME\t\tCLIENT")
         for p in list_process:
+            list_vdom=None
             ip = dict_pid_ip[p.pid]['ip']
             port = dict_pid_ip[p.pid]['port']
             client = dict_pid_ip[p.pid]['client']
+            if('vdom' in dict_pid_ip[p.pid]): list_vdom = dict_pid_ip[p.pid]['vdom']
             if(not p.is_alive()):
                 del dict_pid_ip[p.pid]
-                p = mp.Process(name=client+"_"+ip+":"+str(port),target=execute_process,args=(ip, port, logstash, return_dict, username, password))
+                p = mp.Process(name=client+"_"+ip+":"+str(port),target=execute_process,args=(ip, port, logstash, list_vdom, return_dict, username, password))
                 p.daemon=True
                 p.start()
-                dict_pid_ip.update({ p.pid: {"ip":ip, "port":port, "client":client} })
+                if( list_vdom!=None):
+                    dict_pid_ip.update({ p.pid: {"ip":ip, "port":port, "client":client, "vdom":list_vdom} })
+                else:
+                    dict_pid_ip.update({ p.pid: {"ip":ip, "port":port, "client":client} })
                 list_process[cont]=p
             print( '%02d  %015s:%05s    %06s    %06s   %02s  %010s %010s\t    %010s ' % 
                    (cont , ip , port , p.pid , p.is_alive(), return_dict[ip]['num_int'], return_dict[ip]['status'], return_dict[ip]['enlapsed_time'], client) )
@@ -87,6 +92,8 @@ def init_multiprocessing(list_client, dict_client_ip, logstash, username, passwo
             print(str(ip_json))
             ip = ip_json['ip']
             port = ip_json['port']['ssh']
+            list_vdom = None
+            if 'vdom' in ip_json: list_vdom = ip_json['vdom']
             data_json = {
                 ip : {
                     'status': 'lauched',
@@ -97,10 +104,13 @@ def init_multiprocessing(list_client, dict_client_ip, logstash, username, passwo
                 }
             return_dict.update( data_json )
             # Init by process by IP:PORT
-            p = mp.Process(name=client+"_"+ip+":"+str(port),target=execute_process,args=(ip, port, logstash, return_dict, username, password))
+            p = mp.Process(name=client+"_"+ip+":"+str(port),target=execute_process,args=(ip, port, logstash, list_vdom, return_dict, username, password))
             p.daemon = True
             p.start()
-            dict_pid_ip.update({ p.pid: {"ip":ip, "port":port, "client":client} })
+            if list_vdom!=None:
+                dict_pid_ip.update({ p.pid: {"ip":ip, "port":port, "client":client, "vdom": list_vdom} })
+            else:
+                dict_pid_ip.update({ p.pid: {"ip":ip, "port":port, "client":client} })
             list_process_running.append(p)
     print("--> [  END] process are launched.")
     if (enabled_watchdog):
@@ -114,7 +124,7 @@ def init_multiprocessing(list_client, dict_client_ip, logstash, username, passwo
 if __name__ == "__main__":
     print("--> [START] check_cpu_mem.py")
     #list_client_to_execute=["alianza","aje","alexim","babyclubchic","bdo","brexia","crp","ohsjdd","comexa","continental","cosapi","cpal","disal","dispercol","divemotors","egemsa","enapu","famesa","fibertel","filasur","gomelst","happyland","imm","ind_marique","ifreserve","ingenyo","itochu","la_llave","lab_hofarm","labocer","mastercol","movilmax","orval","proinversion","san_silvestre","santo_domingo","socios_en_salud","supra","thomas_greg","trofeos_castro","uladech","univ_per_union","valle_alto","zinsa","upch","tasa","prompe","engie","cdtel"]
-    list_client_to_execute=["alianza","aje"]
+    list_client_to_execute=["yanbal"]
     #list_client_to_execute=["alianza","aje","alexim","babyclubchic","bdo","brexia","crp","ohsjdd","comexa","continental","cosapi","cpal","disal","dispercol","divemotors","egemsa","enapu","famesa","fibertel","filasur","gomelst","happyland","imm","ind_marique","ifreserve","la_llave","lab_hofarm","labocer","mastercol","movilmax","proinversion","san_silvestre","santo_domingo","socios_en_salud","supra","thomas_greg","trofeos_castro","univ_per_union","zinsa","upch","tasa","prompe","engie","cdtel"]
     init_multiprocessing(list_client_to_execute, dict_client_ip, logstash, username, password)
 #########################################################################################
